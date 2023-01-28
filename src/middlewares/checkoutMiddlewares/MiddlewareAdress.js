c9926e93249980ab33e368ddc8706cb12ff5c6c9
teuprimo <vinicius.brandaot@gmail.com>
2023-01-28 17:32:21 -0300

    fixed get route for shipment info

--- src/middlewares/checkoutMiddlewares/MiddlewareAdress.js
+++ src/middlewares/checkoutMiddlewares/MiddlewareAdress.js
@@ -1,9 +1,9 @@
-import { adressSchema } from "../../models/CheckoutSchemas/adressSchema.js"
-export function adressSchemaValidation(req, res, next) {
+import { adressSchemaPost, adressSchemaGet } from "../../models/CheckoutSchemas/adressSchema.js"
 
+export function adressPostSchemaValidation(req, res, next) {
     const { streetName, houseNumber, postalCode, details, id } = req.body;
     
-    const { error } = adressSchema.validate({ streetName, houseNumber, postalCode, details, id }, { abortEarly: false });
+    const { error } = adressSchemaPost.validate({ streetName, houseNumber, postalCode, details, id }, { abortEarly: false });
   
     if (error) {
       const errors = error.details.map((detail) => detail.message);
@@ -13,3 +13,15 @@ export function adressSchemaValidation(req, res, next) {
     next();
   }
   
+  export function adressGetSchemaValidation(req, res, next) {
+    const { id } = req.body;
+    
+    const { error } = adressSchemaGet.validate({ id }, { abortEarly: false });
+  
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      return res.status(400).send(errors);
+    }
+  
+    next();
+  }
--- src/models/CheckoutSchemas/adressSchema.js
+++ src/models/CheckoutSchemas/adressSchema.js
@@ -1,9 +1,14 @@
 import joi from "joi";
 
-export const adressSchema = joi.object({
+export const adressSchemaPost = joi.object({
     streetName: joi.string().required().min(3),
     houseNumber: joi.number().required(),
     postalCode: joi.number().required(),
-    details: joi.string().required(),
+    details: joi.string(),
+    id: joi.string().required(),
+});
+
+
+export const adressSchemaGet = joi.object({
     id: joi.string().required(),
 });
